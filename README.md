01366f17afbe81a97b11938a64c66f032f658bdc
mickael.pezzoni <mickael.pezzoni@groupe-orqual.com>
2021-06-20 15:46:14 +0200

    fix manifest

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# RunApi
+# api-doc-interpreter
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.0.4.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/runApi'),
+      dir: require('path').join(__dirname, './coverage/api-doc-interpreter'),
       subdir: '.',
       reporters: [
         { type: 'html' },
